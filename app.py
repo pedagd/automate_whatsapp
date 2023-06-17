a3cdba7aff0fe912a197ee3f9b30f31d7c54566a
pedagd <136911193+pedagd@users.noreply.github.com>
2023-06-17 21:05:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ def reply():
     
     response = MessagingResponse()
                       
-    response.message("Hello Durga");
+    response.message("Hi, Thanks for reaching Local Directory Service.\n Choose from the options below:"
+                    "\n\n*Type*\n\n 1️⃣ - Water Softener \n 2️⃣ - Bike Puncture Service \n 3️⃣ - House Keeping Service")
+        
     return str(response)
 
 if __name__ == "__main__":
