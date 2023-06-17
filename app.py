d45891860edb535932a6d9b2609645f9fad724ab
pedagd <136911193+pedagd@users.noreply.github.com>
2023-06-17 22:50:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,30 +15,17 @@ app = Flask(__name__)
 def reply():
     text = request.form.get("Body")
     number = request.form.get("From")
-    #number = number.replace("whatsapp:","")[:-2]
+   
     response = MessagingResponse()
     user = users.find_one({"number": number})
     if bool(user) == False:        
         response.message("Hi, Thanks for reaching Local Directory Service.\n Choose from the options below:"
                     "\n\n*Type*\n\n 1️⃣ - Water Softener \n 2️⃣ - Bike Puncture Service \n 3️⃣ - House Keeping Service")
         users.insert_one({"number": number, "status":"main", "messages":[]})
-    elif user["status"] == "main":      
-        try:
-            option = int(text)
-        except:
-            response.message("Please enter a valid response")
-            return str(response)            
-        if option == 1:
-            contact = contacts.find_one({"name": "Water Softener"})
-            if bool(contact) == True:
-                response.message("Name:{0}\nNumber:{1}\nRemarks:{2}\nRating:{3}".format(
-                contact["name"], contact["number"], contact["remarks"], contact["ratings"]))
-            else:
-                response.message("Unable to find contact")
     else:
         response.message("Hi {0}, Thanks for reaching Local Directory Service.\n Choose from the options below:"
                     "\n\n*Type*\n\n 1️⃣ - Water Softener \n 2️⃣ - Bike Puncture Service \n 3️⃣ - House Keeping Service".format(user["name"]))
-        
+
     return str(response)
     
     
