4348724cbf9d61817145804fd27303261360e162
pedagd <136911193+pedagd@users.noreply.github.com>
2023-06-18 11:04:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,20 +18,17 @@ def reply():
     number = number.replace("whatsapp:", "")
 
     msg = "Hi {0}, Thanks for reaching Local Directory Service. \n Choose from the options below:\n\n*Type*\n\n"
-    
-    
     response = MessagingResponse()
     
     for x in contacts.find():
         msg += "*{0}* - {1}\n".format(x["slno"],x["type"])    
     
-    #response.message("Hi {0}, Thanks for reaching Local Directory Service. \n Choose from the options below:"
-    #                       "\n\n*Type*\n\n {1}".format(number,msg))
     response.message(msg)
     
     try:
        option = int(text)
     except:
+       response.message.clear()
        response.message("Hi {0}, Thanks for reaching Local Directory Service. \n Choose from the options below:"
                            "\n\n*Type*\n\n {1}".format(number,msg))
        return str(response)
@@ -40,6 +37,7 @@ def reply():
         try:            
             contact = contacts.find_one({"slno": option})
             if bool(contact) == True:
+                response.message.clear()
                 response.message("Name:{0}\nNumber:{1}\nRemarks:{2}\nRating:{3}".format(
                 contact["name"], contact["number"], contact["remarks"], contact["ratings"]))
                 return str(response)
@@ -50,8 +48,7 @@ def reply():
             response.message("Unable to find contact")
             return str(response)
     else:
-        response.message("Hi {0}, Thanks for reaching Local Directory Service. \n Choose from the options below:"
-                           "\n\n*Type*\n\n {1}".format(number,msg))
+        response.message(msg)
     
     return str(response)
     
