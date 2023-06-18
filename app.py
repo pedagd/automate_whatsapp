556a70fcc62b3ac6124576a917c15d22172baec3
pedagd <136911193+pedagd@users.noreply.github.com>
2023-06-18 11:22:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ def reply():
     number = request.form.get("From")
     number = number.replace("whatsapp:", "")
 
-    msg = "Hi {0}, Thanks for reaching Local Directory Service. \n Choose from the options below:\n\n*Type*\n\n"
+    msg = "Hi {0}, Thanks for reaching Local Directory Service. \n Choose from the options below:\n\n*Type*\n\n".format(number)
     response = MessagingResponse()
-    
+    options_msg = ""
     for x in contacts.find():
-        msg += "*{0}* - {1}\n".format(x["slno"],x["type"])
-    
-    except_msg = "Please enter a valid option \n Choose from the below options\n{0}".format(msg)
+        options_msg += "*{0}* - {1}\n".format(x["slno"],x["type"])
+
+    msg = msg+options_msg
+    except_msg = "Please enter a valid option \n Choose from the below options\n{0}".format(options_msg)
     
     response.message(msg)
     
@@ -36,8 +37,7 @@ def reply():
     if bool(option) == True:
         try:            
             contact = contacts.find_one({"slno": option})
-            if bool(contact) == True:
-                
+            if bool(contact) == True:                
                 response.message("Name:{0}\nNumber:{1}\nRemarks:{2}\nRating:{3}".format(
                 contact["name"], contact["number"], contact["remarks"], contact["ratings"]))
                 return str(response)
